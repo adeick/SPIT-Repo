5c43338a86fedf902dc971ffb85f4f1a27d142c9
adeick <37886715+adeick@users.noreply.github.com>
2020-02-05 12:05:33 -0600

    Successfully Loop Animations & Destroy GameObjects

--- Transportation-Stress/Assets/Scripts/SceneManagerScript.cs
+++ Transportation-Stress/Assets/Scripts/SceneManagerScript.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    [SerializeField] private GameObject josh;
     public Animator joshAnimator;
     [SerializeField] private float joshWalkDelay = 2f;
+    [SerializeField] private float joshDestroyDelay = 10f;
+    [SerializeField] private GameObject louise;
     public Animator louiseAnimator;
     [SerializeField] private float louiseWalkDelay = 3f;
+    [SerializeField] private float louiseDestroyDelay = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         Invoke("JoshLeaveElevator", joshWalkDelay);
         Invoke("LouiseLeaveElevator", louiseWalkDelay);
+        Destroy(josh, joshDestroyDelay);
+        Destroy(louise, louiseDestroyDelay);
     }
 
     // Update is called once per frame
