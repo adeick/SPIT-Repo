86c2c03a553e618437c98f525f33a7dc5f0d7796
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-02-04 20:48:15 -0600

    Several Animations for Louise and Josh
    Two bystanders now have reasonably good animations to exit the elevator.
    Additionally added a SceneManager to help organize movement across the scene.

---
+++ Transportation-Stress/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public Animator joshAnimator;
+    [SerializeField] private float joshWalkDelay = 2f;
+    public Animator louiseAnimator;
+    [SerializeField] private float louiseWalkDelay = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("JoshLeaveElevator", joshWalkDelay);
+        Invoke("LouiseLeaveElevator", louiseWalkDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void JoshLeaveElevator()
+    {
+       joshAnimator.SetTrigger("LeaveElevator");
+    }
+
+    public void LouiseLeaveElevator()
+    {
+        louiseAnimator.SetTrigger("LeaveElevator");
+    }
+}
