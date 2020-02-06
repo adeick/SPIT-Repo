9fe4263762afce39f2f127d481e2504b7ca8fd3c
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-02-05 21:26:58 -0600

    Implemented a FloorDisplay
    2 Digit, 10 segment display.
    Shows G for Ground Floor and B for Basement (Not sure how to differentiate S from 5 if we want to say Subway)
    
    Additionally added a small spotlight to highlight ElevatorDisplay

---
+++ Transportation-Stress/Assets/Scripts/SevenSegmentDisplay.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SevenSegmentDisplay : MonoBehaviour
+{   
+    MeshRenderer mesh;
+    public bool[] ProgrammingTable = new bool[16]; //Basement is Position 15
+    public Color onColor;
+    public Color offColor;
+    private int floorLevel = 15; //Starts on the 14th floor and goes to zero (15 floors) Basement is position 15 in array
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRenderer mesh = gameObject.GetComponent<MeshRenderer>();
+        mesh.material.color = offColor;
+        Invoke("Count", 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Count(){
+        MeshRenderer mesh = gameObject.GetComponent<MeshRenderer>();
+        floorLevel--;
+        if(ProgrammingTable[floorLevel]){
+            mesh.material.color = onColor;
+        }
+        else{
+            mesh.material.color = offColor;
+        }
+
+        if(floorLevel > 0){
+            Invoke("Count", 2f);
+        }
+        else{
+            Invoke("Basement", 2f);
+        }
+    }
+
+    public void Basement(){
+         MeshRenderer mesh = gameObject.GetComponent<MeshRenderer>();
+            
+        if(ProgrammingTable[15]){
+            mesh.material.color = onColor;
+        }
+        else{
+            mesh.material.color = offColor;
+        }
+    }
+}
