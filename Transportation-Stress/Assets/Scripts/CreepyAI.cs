84bcc3c8f0c357b63a82aa8b141723e143446a34
adeick <37886715+adeick@users.noreply.github.com>
2020-02-06 21:11:48 -0600

    Created NavMesh and Enemy Pathing

---
+++ Transportation-Stress/Assets/Scripts/CreepyAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CreepyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float stoppingRange = 5f;
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(distanceToTarget > stoppingRange){
+            navMeshAgent.SetDestination(target.position);
+        }   
+        else{
+            navMeshAgent.ResetPath();
+        }
+    }
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, stoppingRange);
+    }
+}
